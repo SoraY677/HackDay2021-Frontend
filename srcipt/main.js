d49066ed40b22695e454987d76aa8a061a920344
SoraY677 <ys4080@icloud.com>
2021-03-20 12:52:17 +0900

    [update]live2D導入

--- srcipt/main.js
+++ srcipt/main.js
@@ -1,1 +1,14 @@
-console.log('moge')
+import {
+	Manager
+} from '../resources/Live2D/OriginalLib/Manager.js'
+
+const canvas = document.getElementById('model_canvas')
+const modelSetting = {
+	'modelRootDir': '../resources/Live2D//Model/Hiyori/',
+	'modelJson': 'Hiyori.model3.json',
+	'modelMoc': 'Hiyori.moc3'
+}
+
+const enter = new Manager(canvas, modelSetting)
+
+enter.switchLipSync()
